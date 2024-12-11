588b41b370e86e7c7a7022980a31155f348899ca
csabahi <sabahi.camron@gmail.com>
2024-12-11 09:14:55 -0500

    saving before transfering into soccer_env repo

--- kicker.py
+++ kicker.py
@@ -1,4 +1,15 @@
 import numpy as np
 from .agents import Agent
+from .agents import Humanoid
 import six
 from gym import spaces
+import os
+
+def mass_center(mass, xpos):
+    return (np.sum(mass * xpos, 0) / np.sum(mass))[0]
+
+class HumanoidKicker(Humanoid):
+
+    def __init__(self, agent_id, xml_path=None):
+        if xml_path is None:
+            xml_path = os.path.join(os.path.dirname(__file__), "assets", "humanoid", "humanoid.xml" )
